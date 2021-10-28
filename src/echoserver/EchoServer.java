d638122b7a15beb3217c24d56f41a50bbf36220f
krug0102 <krug0102@morris.umn.edu>
2021-10-28 10:20:57 -0500

    Created EchoServer.java Implemented threads into EchoServer.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,25 +7,60 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+    // REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
+    public static final int PORT_NUMBER = 6013;
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
-		}
-	}
+    public static void main(String[] args) {
+        EchoServer server = new EchoServer();
+        server.start();
+    }
+
+    private void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+            while (true) {
+
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected");
+
+                Thread clientThread = new Thread(() -> {
+                    int i;
+
+                    // socket streams
+                    try {
+                        InputStream streamFromSocket = socket.getInputStream();
+                        OutputStream streamToSocket = socket.getOutputStream();
+                        // while loop that continuously reads one byte from
+                        // the socket, and writes one byte to the socket.
+                        while ((i = streamFromSocket.read()) != -1)
+                        // read one byte from the socket
+                        {
+                            // write one byte to the socket
+                            streamToSocket.write(i);
+                            streamToSocket.flush();
+
+                        }
+
+                        socket.shutdownOutput();
+
+                        // Close the client socket since we're done.
+                        socket.close();
+
+                        System.out.println("Client disconnected.");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                clientThread.start();
+
+            }
+
+            // *Very* minimal error handling.
+        } catch (Exception ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
